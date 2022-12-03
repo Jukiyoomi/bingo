de0a6528f148ec526733b21845f748cb6bd5eb80
Jukiyoomi <jnguessan2003@gmail.com>
2022-12-03 21:05:03 +0100

    register route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Register from "./pages/Register";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/"/>
+				<Route path="/" element={<Register/>}/>
 				<Route path="/game"/>
 			</Routes>
 		</Router>
