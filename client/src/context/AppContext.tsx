99e39edec527235b306bfe45d6736662f433e9de
Jukiyoomi <jnguessan2003@gmail.com>
2022-12-03 21:28:32 +0100

    user of username hook

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,18 +1,23 @@
 import React, {createContext, useContext, useMemo} from "react";
+import useUsername from "../../hooks/useUsername";
 
 interface IAppContext {
-
+	username: string | null,
+	setUsername: React.Dispatch<React.SetStateAction<string | null>>
 }
 
 const AppContext = createContext<IAppContext | null>(null)
 
 const AppProvider = ({children}: { children: React.ReactNode }) => {
 	/******************** STATES ********************/
-
+	const {username, setUsername} = useUsername()
 	/******************** FUNCTIONS ********************/
 
 	/******************** VALUE ********************/
-	const value = useMemo(() => ({}), [])
+	const value = useMemo(() => ({
+		username,
+		setUsername
+	}), [username])
 
 	/******************** RETURN ********************/
 	return (
