0e906074b426f6c9c02b2725a1e36bac19cb4fa5
Jukiyoomi <jnguessan2003@gmail.com>
2022-12-03 21:06:49 +0100

    game page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Register from "./pages/Register";
+import Game from "./pages/Game";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Register/>}/>
-				<Route path="/game"/>
+				<Route path="/game" element={<Game/>}/>
 			</Routes>
 		</Router>
 	)
