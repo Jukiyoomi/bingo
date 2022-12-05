3924e77a97a9e6cc70a53a82dd50039f6e14cb73
Jukiyoomi <jnguessan2003@gmail.com>
2022-12-05 19:31:44 +0100

    button "start game" for the chief of the game

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -5,7 +5,7 @@ import Button from "../components/Button";
 import Players from "../components/Players";
 
 const Game = () => {
-	const {username, connect, players} = useAppContext()
+	const {username, connect, players, getReady, currentPlayer} = useAppContext()
 	const navigate = useNavigate()
 
 	useEffect(() => {
@@ -13,6 +13,7 @@ const Game = () => {
 		if (!username) navigate("/")
 		else connect()
 	}, [])
+
 
 	return (
 		<div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
@@ -27,9 +28,19 @@ const Game = () => {
 			<div className="max-w-[800px] mx-auto mt-8 mb-0 max-w-md flex flex-col items-start gap-4">
 				<Players/>
 				<div className="flex items-center justify-end">
-					<Button type="submit">
-						Get Ready
-					</Button>
+					{currentPlayer && currentPlayer.ready ?
+						(
+							currentPlayer.role === "chief" &&
+							<Button
+								className={players.every(player => player.ready) ? "" : "pointer-events-none bg-gray-400"}>
+								Start The Game
+							</Button>
+						)
+						:
+						<Button onClick={getReady}>
+							Get Ready
+						</Button>
+					}
 				</div>
 			</div>
 		</div>
