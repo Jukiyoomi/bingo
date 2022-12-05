5d19659523ea0f01a77b848206affcc658fb8a79
Jukiyoomi <jnguessan2003@gmail.com>
2022-12-05 13:14:21 +0100

    connect client to server using socket

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,22 +1,34 @@
 import React, {createContext, useContext, useMemo} from "react";
 import useUsername from "../../hooks/useUsername";
+import useSocket from "../../hooks/useSocket";
+import {Socket} from "socket.io-client";
 
 interface IAppContext {
 	username: string | null,
-	setUsername: React.Dispatch<React.SetStateAction<string | null>>
+	setUsername: React.Dispatch<React.SetStateAction<string | null>>,
+	socket: Socket
 }
 
 const AppContext = createContext<IAppContext | null>(null)
 
+const socketOptions = {
+	autoConnect: false,
+	reconnection: false,
+	withCredentials: true,
+}
+
 const AppProvider = ({children}: { children: React.ReactNode }) => {
 	/******************** STATES ********************/
 	const {username, setUsername} = useUsername()
+	const socket = useSocket("http://localhost:4000", socketOptions)
+
 	/******************** FUNCTIONS ********************/
 
 	/******************** VALUE ********************/
 	const value = useMemo(() => ({
 		username,
-		setUsername
+		setUsername,
+		socket
 	}), [username])
 
 	/******************** RETURN ********************/
--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useAppContext} from "../context/AppContext";
+import {useNavigate} from "react-router-dom";
 
 const Game = () => {
+	const {socket, username} = useAppContext()
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (!username) navigate("/")
+	}, [])
+
+	useEffect(() => {
+		socket.connect()
+	}, [])
+
 	return (
 		<div>
 			Game
