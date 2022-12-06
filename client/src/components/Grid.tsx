482c4f8fbab9d0f169b4f2b3708a1404defc250a
Jukiyoomi <jnguessan2003@gmail.com>
2022-12-06 20:22:47 +0100

    function to send found number to server

--- client/src/components/Grid.tsx
+++ client/src/components/Grid.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import {IRowProps} from "../../../interfaces";
 import {useAppContext} from "../context/AppContext";
+import {toast} from "react-toastify";
 
 const Grid = () => {
-	const {grid} = useAppContext()
+	const {grid, currentNumber, emitNumber} = useAppContext()
+
+	const clickNumber = (value: number) => {
+		if (!currentNumber) return
+		if (currentNumber !== value) return toast.error("This is not the good number...")
+		emitNumber(value)
+	}
 
 	return (
 		<article
 			className="mx-auto max-w-[600px] h-[600px] grid grid-rows-5 bg-white shadow-lg mb-3"
 		>
 			{
-				grid.map((row: IRowProps[]) => (
-					<ul className="w-full h-full flex">
+				grid.map((row: IRowProps[], id) => (
+					<ul className="w-full h-full flex" key={id}>
 						{
 							row.map(({value, found}: IRowProps) =>
 								<li
+									onClick={() => clickNumber(value)}
+									key={value}
 									value={value}
 									className={`flex-1 flex justify-center items-center border cursor-pointer transition hover:bg-gray-400 
 											${found && "bg-blue-500 text-white pointer-events-none"}`}
