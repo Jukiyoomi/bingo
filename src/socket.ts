5ba9f4904f28446ab024b2945eedf6f74efdd93d
Jukiyoomi <jnguessan2003@gmail.com>
2022-12-07 20:15:39 +0100

    bug fix, error when only one player who is the chief

--- src/socket.ts
+++ src/socket.ts
@@ -90,7 +90,7 @@ export default class ServerSocket {
 			this.players = this.players.filter(player => player.socketId !== socket.id)
 
 			// Set a new chief if the player to be removed is the chief
-			if (removingPlayer && removingPlayer.role === "chief") {
+			if (this.players.length > 0 && removingPlayer && removingPlayer.role === "chief") {
 				this.players[0].role = "chief"
 			}
 
