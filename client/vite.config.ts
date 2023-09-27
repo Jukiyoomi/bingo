1f25c7167614e8500055e15411d2405976693d18
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-09-27 11:47:12 +0200

    created some paths aliases for front and back #11

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@hooks": path.resolve(__dirname, "./hooks"),
+			"@store": path.resolve(__dirname, "./store"),
+			"@common": path.resolve(__dirname, "../common"),
+		}
+	}
 })
